cbfd3da1fdd955c734035b2d5010a392af101334
Sean Geoghegan <seangeo@gmail.com>
2025-04-12 21:59:52 +0930

    Move exercise 1 to separate module.

---
+++ src/nature_of_code/ex_0_1/mod.rs
@@ -0,0 +1,61 @@
+use nannou::prelude::*;
+
+pub fn run() {
+    nannou::app(model)
+        .update(update)
+        .simple_window(view)
+        .run();
+}
+
+struct Model {
+    x: f32,
+    y: f32,
+}
+
+fn model(_app: &App) -> Model {
+    Model { x: 0., y: 0. }
+}
+
+fn update(_app: &App, model: &mut Model, _update: Update) {
+    let step: f32 = nannou::rand::random();
+
+    if step >= 0.7 {
+        model.x -= 1.;
+        model.y -= 1.;
+    } else if step >= 0.6 {
+        model.x -= 1.;
+    } else if step >= 0.5 {
+        model.x -= 1.;
+        model.y += 1.;
+    } else if step >= 0.4 {
+        model.y += 1.;
+    } else if step >= 0.3 {
+        model.x += 1.;
+        model.y += 1.;
+    } else if step >= 0.2 {
+        model.x += 1.;
+    } else if step >= 0.1 {
+        model.x += 1.;
+        model.y -= 1.;
+    } else {
+        model.y -= 1.;
+    }
+}
+
+fn view(app: &App, model: &Model, frame: Frame) {
+    let draw = app.draw();
+
+    if frame.nth() == 0 {
+
+    draw.background().color(WHITE);
+    }
+
+    draw.rect()
+        .x(model.x)
+        .y(model.y)
+        .w(1.)
+        .h(1.)
+        .color(BLACK);
+
+    draw.to_frame(app, &frame).unwrap();
+}
