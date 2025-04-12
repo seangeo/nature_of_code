294af4f05584ac3bf0a96a9bccfc90086993423b
Sean Geoghegan <seangeo@gmail.com>
2025-04-12 22:06:41 +0930

    Enable exercise selection as a command line arg.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
 pub mod nature_of_code;
 
 fn main() {
-    nature_of_code::ex_0_1::run();
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() != 2 {
+        eprintln!("Usage: {} <exercise>", args[0]);
+        std::process::exit(1);
+    }
+
+    let exercise = &args[1];
+    
+    match exercise.as_str() {
+        "ex_0_1" => nature_of_code::ex_0_1::run(),
+        _ => {
+            eprintln!("Unknown exercise: {}", exercise);
+            std::process::exit(1);
+        }
+    }
 }
