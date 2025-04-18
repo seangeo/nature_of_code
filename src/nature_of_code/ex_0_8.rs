2b6b6137496697d40b32ad3f6fc18a8796304a69
Sean Geoghegan <seangeo@gmail.com>
2025-04-18 09:41:10 +0930

    Add rayon for parallel image building.

--- src/nature_of_code/ex_0_8.rs
+++ src/nature_of_code/ex_0_8.rs
@@ -6,6 +6,7 @@ use nannou::prelude::*;
 use nannou::wgpu::Texture;
 use nannou::{image::ImageBuffer, noise::NoiseFn};
 use nannou_egui::{self, egui, FrameCtx};
+use rayon::prelude::*;
 
 pub fn init(_app: &App) -> Box<dyn Exercise> {
     let image = ImageBuffer::new(0, 0);
@@ -35,6 +36,32 @@ impl Model {
         let c = map_range(n, -1., 1., 0, 255) as u8;
         nannou::image::Rgba([c, c, c, 100])
     }
+
+    fn build_image(&mut self, width: u32, height: u32) {
+        if self.build_image {
+            let time = Instant::now();
+
+            let coords: Vec<(u32, u32)> = (0..height)
+                .flat_map(|y| (0..width).map(move |x| (x, y)))
+                .collect();
+
+            let pixels: Vec<nannou::image::Rgba<u8>> = coords
+                .par_iter()
+                .map(|&(x, y)| self.pixel_for(x, y))
+                .collect();
+
+            let mut image = ImageBuffer::new(width, height);
+            for (i, pixel) in pixels.iter().enumerate() {
+                let x = (i % width as usize) as u32;
+                let y = (i / width as usize) as u32;
+                image.put_pixel(x, y, *pixel);
+            }
+
+            self.image = DynamicImage::ImageRgba8(image);
+            self.build_image = false;
+            debug!("Ex_0_8 build_image time={:?}", time.elapsed());
+        }
+    }
 }
 
 impl Exercise for Model {
@@ -54,17 +81,7 @@ impl Exercise for Model {
             }
         });
 
-        if self.build_image {
-            let time = Instant::now();
-            let image = ImageBuffer::from_fn(
-                app.window_rect().w() as u32,
-                app.window_rect().h() as u32,
-                |x, y| self.pixel_for(x, y),
-            );
-            self.image = DynamicImage::ImageRgba8(image);
-            self.build_image = false;
-            debug!("Ex_0_8 build_image time={:?}", time.elapsed());
-        }
+        self.build_image(app.window_rect().w() as u32, app.window_rect().h() as u32);
     }
 
     fn draw(&self, app: &App, frame: &Frame) {
