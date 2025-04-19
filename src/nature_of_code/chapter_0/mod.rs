d2163b754dc00276520d9bd873bec1b3286de02e
Sean Geoghegan <seangeo@gmail.com>
2025-04-19 14:11:44 +0930

    Update: Improve exercise names to be more descriptive

--- src/nature_of_code/chapter_0/mod.rs
+++ src/nature_of_code/chapter_0/mod.rs
@@ -12,35 +12,35 @@ pub mod ex_0_10;
 
 pub const EXERCISES: [ExerciseInfo; 8] = [
     ExerciseInfo {
-        name: "Exercise 0.1",
+        name: "0.1 Random Walk",
         init_fn: ex_0_1::init,
     },
     ExerciseInfo {
-        name: "Exercise 0.3",
+        name: "0.3 Mouse Following",
         init_fn: ex_0_3::init,
     },
     ExerciseInfo {
-        name: "Exercise 0.4",
+        name: "0.4 Gaussian Splatter",
         init_fn: ex_0_4::init,
     },
     ExerciseInfo {
-        name: "Exercise 0.5",
+        name: "0.5 Gaussian Distribution",
         init_fn: ex_0_5::init,
     },
     ExerciseInfo {
-        name: "Exercise 0.6",
+        name: "0.6 Quadratic Distribution",
         init_fn: ex_0_6::init,
     },
     ExerciseInfo {
-        name: "Exercise 0.7",
+        name: "0.7 Perlin Noise Motion",
         init_fn: ex_0_7::init,
     },
     ExerciseInfo {
-        name: "Exercise 0.8",
+        name: "0.8 Noise Image Generator",
         init_fn: ex_0_8::init,
     },
     ExerciseInfo {
-        name: "Exercise 0.10",
+        name: "0.10 3D Noise Terrain",
         init_fn: ex_0_10::init,
     },
 ];
