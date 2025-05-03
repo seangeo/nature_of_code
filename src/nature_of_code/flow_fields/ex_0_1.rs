012fad46bb34b7945300639c96f0bf944a8aa799
Sean Geoghegan <seangeo@gmail.com>
2025-05-03 14:36:48 +0930

    Add UI to control noise parameters in flow field.

--- src/nature_of_code/flow_fields/ex_0_1.rs
+++ src/nature_of_code/flow_fields/ex_0_1.rs
@@ -1,6 +1,6 @@
 use crate::nature_of_code::{exercise::Exercise, noise_config::NoiseConfig};
 use nannou::{noise::NoiseFn, prelude::*};
-use nannou_egui::{self, FrameCtx};
+use nannou_egui::{self, egui, FrameCtx};
 
 pub fn init(app: &App) -> Box<dyn Exercise> {
     Box::new(model(app))
@@ -9,6 +9,7 @@ pub fn init(app: &App) -> Box<dyn Exercise> {
 struct Model {
     flow_lines: Vec<FlowLine>,
     noise_config: NoiseConfig,
+    clear: bool,
 }
 
 impl Model {
@@ -56,26 +57,55 @@ impl FlowLine {
     }
 }
 
+fn generate_flow_lines(width: f32, height: f32) -> Vec<FlowLine> {
+    (0..5000).map(|_| FlowLine::new(width, height)).collect()
+}
+
 fn model(app: &App) -> Model {
     let window = app.window_rect();
     let width = window.w();
     let height = window.h();
 
-    let flow_lines: Vec<FlowLine> = (0..5000).map(|_| FlowLine::new(width, height)).collect();
-
     Model {
-        flow_lines,
+        flow_lines: generate_flow_lines(width, height),
         noise_config: NoiseConfig::default(),
+        clear: false,
     }
 }
 
 impl Exercise for Model {
-    fn update(&mut self, app: &App, _update: Update, _ui_ctx: &FrameCtx) {
+    fn update(&mut self, app: &App, _update: Update, ui_ctx: &FrameCtx) {
+        if self.clear {
+            self.clear = false;
+        }
+
+        egui::TopBottomPanel::bottom("Settings").show(&ui_ctx, |ui| {
+            ui.with_layout(egui::Layout::left_to_right(egui::Align::TOP), |ui| {
+                ui.allocate_ui_with_layout(
+                    egui::Vec2::new(0., 0.),
+                    egui::Layout::top_down(egui::Align::LEFT),
+                    |ui| {
+                        if self.noise_config.ui(ui) {
+                            self.clear = true;
+                        }
+                    },
+                );
+            })
+        });
+
+        if self.clear {
+            self.flow_lines = generate_flow_lines(app.window_rect().w(), app.window_rect().h());
+        }
+
         self.update_flow_lines(app.time as f64);
     }
 
     fn draw(&self, app: &App, frame: &Frame) {
         let draw = app.draw();
+
+        if self.clear {
+            draw.background().color(WHITE);
+        }
 
         for flow_line in &self.flow_lines {
             draw.line()
